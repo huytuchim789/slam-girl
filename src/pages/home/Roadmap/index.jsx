5b648cc4fedc1337e6a72806f382482498962a64
phamlam2k <mailam1309@gmail.com>
2022-02-24 20:18:43 +0700

    fix responsive

--- src/pages/home/Roadmap/index.jsx
+++ src/pages/home/Roadmap/index.jsx
@@ -8,32 +8,34 @@ export default function Roadmap() {
   return (
     <div className="roadmap">
       <div className="roadmap__container">
-        <h1 className="roadmap__container__header">THE ROADMAP</h1>
-        <div className="roadmap__container__subheader">
-          Euismod sit iaculis ultrices in elit sed facilisis aliquet. Pharetra
-          porttitor lacus eu viverra tellus morbi euismod quisque adipiscing. Id
-          id vulputate sed feugiat tellus. Leo ac in purus in. Semper praesent a
-          leo maecenas.
+        <div className='roadmap__container-content'>
+          <h1 className="roadmap__container__header">THE ROADMAP</h1>
+          <div className="roadmap__container__subheader">
+            Euismod sit iaculis ultrices in elit sed facilisis aliquet. Pharetra
+            porttitor lacus eu viverra tellus morbi euismod quisque adipiscing. Id
+            id vulputate sed feugiat tellus. Leo ac in purus in. Semper praesent a
+            leo maecenas.
+          </div>
+          <div className="roadmap__timeline">
+            {maps.map((map) => (
+              <div className="roadmap__item">
+                <h3 className="roadmap__item__phase">{map.phase}</h3>
+                {Array.isArray(map.content) ? (
+                  <ul>
+                    {map.content.map((c) => (
+                      <li className="roadmap__item_element">{c}</li>
+                    ))}
+                  </ul>
+                ) : (
+                  <div className="roadmap__item_element">{map.content}</div>
+                )}
+                <span className="circle" />
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="roadmap__timeline">
-          {maps.map((map) => (
-            <div className="roadmap__item">
-              <h3 className="roadmap__item__phase">{map.phase}</h3>
-              {Array.isArray(map.content) ? (
-                <ul>
-                  {map.content.map((c) => (
-                    <li className="roadmap__item_element">{c}</li>
-                  ))}
-                </ul>
-              ) : (
-                <div className="roadmap__item_element">{map.content}</div>
-              )}
-              <span className="circle" />
-            </div>
-          ))}
-        </div>
+        <img src={roadmapImg} className="roadmap__img" alt="" />
       </div>
-      <img src={roadmapImg} className="roadmap__img" alt="" />
     </div>
   )
 }

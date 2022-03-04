12c51f201d69532e05bc030619dda4e0fe9811cd
Huy Tu <tranhuytu242000@gmail.com>
2022-03-04 11:05:24 +0700

    finished

--- src/components/RightSideBar/index.js
+++ src/components/RightSideBar/index.js
@@ -10,9 +10,9 @@ export default function RightSideBar({ isMobile }) {
   const { onMenu, setOnMenu } = useContext(MenuContext)
   return (
     <section className="right_sidebar">
-      <div className="menu_icon_container">
+      <div className={`menu_icon_container ${onMenu ? 'menu-bg' : ''}`}>
         <img
-          className="menu_icon"
+          className={`menu_icon`}
           src={onMenu ? closeIcon : MenuIcon}
           alt="menu"
           onClick={() => {
@@ -21,37 +21,29 @@ export default function RightSideBar({ isMobile }) {
         />
       </div>
 
-          <span className="right_sidebar__header">Join us</span>
-          <div className="right_sidebar__icon_list">
-            <a href="https://discord.com/" target="_blank">
-              <img
-                alt="img"
-                className="right_sidebar__icon_item"
-                src={discordIcon}
-              />
-            </a>
-            <a href="https://www.instagram.com/" target="_blank">
-              <img
-                alt="img"
-                className="right_sidebar__icon_item"
-                src={instaIcon}
-              />
-            </a>
-            <a href="https://twitter.com/?lang=en" target="_blank">
-              <img
-                alt="img"
-                className="right_sidebar__icon_item"
-                src={twitterIcon}
-              />
-            </a>
-            <a href="https://www.messenger.com/" target="_blank">
-              <img
-                alt="img"
-                className="right_sidebar__icon_item"
-                src={sendIcon}
-              />
-            </a>
-          </div>
+      <span className="right_sidebar__header">Join us</span>
+      <div className="right_sidebar__icon_list">
+        <a href="https://discord.com/" target="_blank">
+          <img
+            alt="img"
+            className="right_sidebar__icon_item"
+            src={discordIcon}
+          />
+        </a>
+        <a href="https://www.instagram.com/" target="_blank">
+          <img alt="img" className="right_sidebar__icon_item" src={instaIcon} />
+        </a>
+        <a href="https://twitter.com/?lang=en" target="_blank">
+          <img
+            alt="img"
+            className="right_sidebar__icon_item"
+            src={twitterIcon}
+          />
+        </a>
+        <a href="https://www.messenger.com/" target="_blank">
+          <img alt="img" className="right_sidebar__icon_item" src={sendIcon} />
+        </a>
+      </div>
     </section>
   )
 }
